3d38db362be9498e4d4fba30f0b086cd31386731
Bart Trzynadlowski <bart.trzy@gmail.com>
2023-03-03 16:49:57 -0800

    More sample images to make testing easier

--- python/web/simulated_image_dispatcher.py
+++ python/web/simulated_image_dispatcher.py
@@ -11,6 +11,7 @@ import PIL
 import platform
 from queue import Queue
 from queue import Empty
+import random
 from typing import Callable
 
 from .image_dispatcher import ImageResult
@@ -21,8 +22,14 @@ class SimulatedImageDispatcherTask:
         super().__init__()
         self._result_queue = Queue(maxsize = 0)
 
-        # Load image to return
-        self._dummy_image = PIL.Image.open("assets/Plissken2.jpg")
+        # Load images to return
+        self._dummy_images = [
+            PIL.Image.open("assets/Plissken2.jpg"),
+            PIL.Image.open("assets/KermitPlissken.jpg"),
+            PIL.Image.open("assets/RickAstley.jpg"),
+            PIL.Image.open("assets/SpaceFarley.jpg"),
+            PIL.Image.open("assets/JoeBiden.jpg")
+        ]
 
     async def run(self):
         print("Starting SIMULATED image dispatcher...")
@@ -43,6 +50,7 @@ class SimulatedImageDispatcherTask:
         return item
 
     async def submit_prompt(self, prompt: str, request_id: str, completion: Callable[[ImageResult], None]):
-        result = ImageResult(request_id = request_id, images = [ self._dummy_image ] * 4)
+        random.shuffle(self._dummy_images)
+        result = ImageResult(request_id = request_id, images = self._dummy_images[0:4])
         await asyncio.sleep(5)  # simulated time delay
         self._result_queue.put(item = (completion, result))
